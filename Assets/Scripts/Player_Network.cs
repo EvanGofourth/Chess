a0a419e886e1e26c73efaea75fdcc50d8d90fa87
Evan Gofourth <evangofourth99@gmail.com>
2019-12-04 18:55:14 -0600

    Initial commit.

---
+++ Assets/Scripts/Player_Network.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class Player_Network : NetworkBehaviour
+{
+    public GameObject camera_holding_object;
+
+    public override void OnStartLocalPlayer()
+    {
+        base.OnStartLocalPlayer();
+    }
+}
